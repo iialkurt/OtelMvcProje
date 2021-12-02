92890e877d01ec8a14440e076cbe48329339fab8
ismail alkurt <ialkurt@hotmail.com>
2021-12-02 18:12:37 +0300

    Add project files.

---
+++ OtelMvcProje/Controllers/HakkimizdaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OtelMvcProje.Models.Entity;
+
+namespace OtelMvcProje.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        DbOtelEntitiess db = new DbOtelEntitiess();
+        public ActionResult Index()
+        {
+            var veriler = db.Hakkimizda.ToList();
+            return View(veriler);
+        }
+    }
+}
---
+++ OtelMvcProje/Controllers/kayitController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    public class kayitController : Controller
+    {
+        // GET: kayit
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult kayitol()
+        {
+            return View();
+        }
+    }
+}
